61f5179b8e59e2e62253739d8440908bfa58529f
Byron <bgva2005@yahoo.com>
2023-07-12 09:19:56 -0500

    feat: initial project mvc

---
+++ PruebaSedemi_00_20230703/PruebaSedemi_00.MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PruebaSedemi_00.MVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
